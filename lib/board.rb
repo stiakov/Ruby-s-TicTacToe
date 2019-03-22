82c0417f3b6ea8dace2ffdad902620319402a2ce
Santiago Torres G <thdealer@gmail.com>
2019-03-21 19:28:01 -0500

    improving board and testing

--- lib/board.rb
+++ lib/board.rb
@@ -4,13 +4,13 @@ class Board
   attr_accessor :boxes
 
   def initialize
-    @boxes = {a1: 'X', a2: 'X', a3: 'X', 
-              b1: 'X', b2: 'X', b3: 'X', 
-              c1: 'X', c2: 'X', c3: 'X'}
+    @boxes = {a1: '1', a2: '2', a3: '3', 
+              b1: '4', b2: '5', b3: '6', 
+              c1: '7', c2: '8', c3: '9'}
     
-    @board = "|#{@boxes[:a1]}|#{@boxes[:a2]}|#{@boxes[:a3]}|
-|#{@boxes[:b1]}|#{@boxes[:b2]}|#{@boxes[:b3]}|
-|#{@boxes[:c1]}|#{@boxes[:c2]}|#{@boxes[:c3]}|"
+    @board = "| #{@boxes[:a1]} | #{@boxes[:a2]} | #{@boxes[:a3]} |
+| #{@boxes[:b1]} | #{@boxes[:b2]} | #{@boxes[:b3]} |
+| #{@boxes[:c1]} | #{@boxes[:c2]} | #{@boxes[:c3]} |"
 
   end
 
@@ -18,20 +18,31 @@ class Board
     puts @board
   end
 
-  def update_board arg, mark
-    case arg
-    when 1 then @boxes[a1 = mark]
-    when 2 then @boxes[a2 = mark]
-    when 3 then @boxes[a3 = mark]
-    when 4 then @boxes[b1 = mark]
-    when 5 then @boxes[b2 = mark]
-    when 6 then @boxes[b3 = mark]
-    when 7 then @boxes[c1 = mark]
-    when 8 then @boxes[c2 = mark]
-    when 9 then @boxes[c3 = mark]
+  def update_board box, mark
+    case box
+    when 1 then @boxes[:a1] = mark
+    when 2 then @boxes[:a2] = mark
+    when 3 then @boxes[:a3] = mark
+    when 4 then @boxes[:b1] = mark
+    when 5 then @boxes[:b2] = mark
+    when 6 then @boxes[:b3] = mark
+    when 7 then @boxes[:c1] = mark
+    when 8 then @boxes[:c2] = mark
+    when 9 then @boxes[:c3] = mark
     else "Un-existent Box"
     end
+    @board = " -----------
+| #{@boxes[:a1]} | #{@boxes[:a2]} | #{@boxes[:a3]} |
+| #{@boxes[:b1]} | #{@boxes[:b2]} | #{@boxes[:b3]} |
+| #{@boxes[:c1]} | #{@boxes[:c2]} | #{@boxes[:c3]} |
+ -----------"
 
+    @board
   end
- 
 end
+
+a = Board.new
+puts a.load_board
+puts a.update_board(5, 'X')
+puts a.update_board(3, 'O')
+puts 'fin'
