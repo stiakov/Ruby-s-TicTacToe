b48dd83a4ecd2ee141252776ad5b07f3311f8ddb
Santiago Torres G <thdealer@gmail.com>
2019-03-23 00:32:44 -0500

    dividing tasks and avoiding repetition

--- lib/game.rb
+++ lib/game.rb
@@ -19,56 +19,63 @@ class Game
     puts ''
     @player_1.move(player_1, @board)
     counter = 1
-    validation = player_1.places.size + player_2.places.size == 8
 
-    until @player_1.winner || @player_2.winner || validation
-      puts ''
-      @board.load_board
-      puts ''
-      puts "#{@player_2.name} make your move"
-      puts ''
+    size_validator = Proc.new { |i, j| i + j > 7 }
 
-      @player_2.move(@player_2, @board)
-      counter += 1
-      chk1 = check_winner(@player_1.places) if counter > 4
+    check_out = proc { |player|
+        winner_is = player.name
+        puts "\nThe winner is #{winner_is}"
+        puts "#{@board.load_board}\n===== Game Over =====\n\n"
+    }
 
-      puts ''
-      @board.load_board
-      puts ''
-      puts "#{@player_1.name} make your move"
-      puts ''
-
-      @player_1.move(@player_1, @board)
-      counter += 1
-      chk2 = check_winner(@player_2.places) if counter > 4
+    stop = false
+    until @player_1.winner || @player_2.winner || stop
       
-      if chk1 || chk2
-        winner_is = chk1 ? @player_1.name : @player_2.name
-        puts "The winner is #{winner_is}"
-        return puts @board.load_board
-      end
+      # PLAYER 2
+      make_your_move(@player_2, @board)
+      counter += 1
+      checker2 = check_winner(@player_2) if counter > 4
+      return check_out.call(@Player_2) if checker2
+      
+      # PLAYER 1
+      make_your_move(@player_1, @board)
+      counter += 1
+      checker1 = check_winner(@player_1) if counter > 4
+      return check_out.call(@player_1) if checker1
+
+      stop = size_validator.call(@player_1.places.size, @player_2.places.size)
+
+      return "Draw, try again!\n===== Game Over =====\n\n" if stop
     end
   end
 
-  def check_winner(places)
-    out = if places.keep_if { |i| i == 1 || i == 2 || i == 3 }.size == 3
+  def make_your_move(player, board)
+    puts ''
+    @board.load_board
+    puts "\n#{player.name} make your move\n"
+    player.move(player, board)
+  end
+
+  def check_winner(player)
+    out = if (player.places & [1, 2, 3]).size == 3
             true
-          elsif places.keep_if { |i| i == 4 || i == 5 || i == 6 }.size == 3
+          elsif (player.places & [4, 5, 6]).size == 3
             true
-          elsif places.keep_if { |i| i == 7 || i == 8 || i == 9 }.size == 3
+          elsif (player.places & [7, 8, 9]).size == 3
             true
-          elsif places.keep_if { |i| i == 1 || i == 4 || i == 7 }.size == 3
+          elsif (player.places & [1, 4, 7]).size == 3
             true
-          elsif places.keep_if { |i| i == 2 || i == 5 || i == 8 }.size == 3
+          elsif (player.places & [2, 5, 8]).size == 3
             true
-          elsif places.keep_if { |i| i == 3 || i == 6 || i == 9 }.size == 3
+          elsif (player.places & [3, 6, 9]).size == 3
             true
-          elsif places.keep_if { |i| i == 1 || i == 5 || i == 9 }.size == 3
+          elsif (player.places & [1, 5, 9]).size == 3
             true
-          elsif places.keep_if { |i| i == 3 || i == 5 || i == 7 }.size == 3
+          elsif (player.places & [3, 5, 7]).size == 3
             true
           else false
           end
+    player.winner = true if out
     out
   end
 end
