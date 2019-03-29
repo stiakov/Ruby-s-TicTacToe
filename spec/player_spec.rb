66c62999e3659633edb285fb3084d7f36afb0f35
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-03-29 15:28:31 -0700

    starter file for game rspec

---
+++ spec/player_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+
+RSpec.describe Player do
+  describe '.move' do
+    let(:surface) {surface.new("\n\n
+              TIC TAC TOE\n
+    ┌─────────┬─────────┬─────────┐
+    │ #{@cells[:a1]} │ Yb   dP │ #{@cells[:c1]} │
+    │ #{@cells[:a2]} │  Yb.dP  │ #{@cells[:c2]} │      #{@player1}: #{@score1}
+    │ #{@cells[:a3]} │  dP'Yb  │ #{@cells[:c3]} │
+    │ #{@cells[:a4]} │ dP   Yb │ #{@cells[:c4]} │
+    ├─────────┼─────────┼─────────┤
+    │ #{@cells[:d1]} │ #{@cells[:e1]} │ #{@cells[:f1]} │
+    │ #{@cells[:d2]} │ #{@cells[:e2]} │ #{@cells[:f2]} │      #{@player2}: #{@score2}
+    │ #{@cells[:d3]} │ #{@cells[:e3]} │ #{@cells[:f3]} │
+    │ #{@cells[:d4]} │ #{@cells[:e4]} │ #{@cells[:f4]} │
+    ├─────────┼─────────┼─────────┤
+    │ #{@cells[:g1]} │ #{@cells[:h1]} │ #{@cells[:i1]} │
+    │ #{@cells[:g2]} │ #{@cells[:h2]} │ #{@cells[:i2]} │      TIE: #{@tie_score}
+    │ #{@cells[:g3]} │ #{@cells[:h3]} │ #{@cells[:i3]} │
+    │ #{@cells[:g4]} │ #{@cells[:h4]} │ #{@cells[:i4]} │
+    └─────────┴─────────┴─────────┘\n\n")}
+    it 'places an user input in board' do
+      player = Player.new('E','X')
+      expected(player.move(player.mark, 2)).to eq[:surface]
+    end
+  end
+end
